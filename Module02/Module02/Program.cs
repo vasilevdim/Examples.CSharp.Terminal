3442c7a064236115f8a89f8e8de60742b57d3b70
vasilevdim <vasilevdim@gmail.com>
2017-02-11 21:49:46 +0300

    Modul 02, Video 01 - Add Comments

---
+++ Module02/Module02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Module02
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//Definirane na promenlivi
+			//Sabirane na chisla
+			//Pisane v consola
+
+
+		}
+	}
+}
