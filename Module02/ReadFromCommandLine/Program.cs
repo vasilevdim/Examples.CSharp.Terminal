eb87543f36045b1097c6548495cde2dc3f9667f7
vasilevdim <vasilevdim@gmail.com>
2017-02-15 22:12:07 +0300

    Modul 2 Video 6 Rabota sas stringove

---
+++ Module02/ReadFromCommandLine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadFromCommandLine
+{
+	class MainClass
+	{
+		public static void Main(string[] args);
+		//Variables Definition
+
+		int a = 5, b = 0;
+		//Input Parameterts
+		Console.WriteLine 
