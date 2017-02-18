50e2749858c845efc549dbd7f110bad827368e72
vasilevdim <vasilevdim@gmail.com>
2017-02-18 23:11:05 +0300

    Modul 02, Video 08

--- TableProject/TableProject/Program.cs
+++ TableProject/TableProject/Program.cs
@@ -10,15 +10,20 @@ namespace TableProject
     {
         static void Main(string[] args)
         {
-            string[] row = new string[3];
-            row[0] = "Red1";
-            row[1] = "Red2";
-            row[2] = "Red3";
-            Console.WriteLine("Masiv row:"+row [0]+", "+row[1]+", "+row [2]+"\n");
-            string[] parse = "1,2,3,4,5,6,7,8,9".Split(',');
-            Console.WriteLine("Broiat na masiva:\n" + "1,2,3,4,5,6,7,8,9\n\ne:" + " "+parse.Length);
-            string list1 = string.Join(";", parse);
-            Console.WriteLine("Noviat string e:\n" + list1 + "\n\n");
+            //Definirane na tabliza
+            string[,] table= new string[2, 3];
+            //Vavejdane na I red
+            table[0, 0] = "Martin";table[0, 1] = "Simeonov"; table[0, 2] = "31";
+
+
+
+            // Vavejdane na II red
+            table[1, 0] = "Simeon"; table[1, 1] = "Martinov"; table[1, 2] = "41";
+            //Pechat
+            Console.WriteLine("Koi red iskate da vidite:");
+
+            int _index = Convert.ToInt32(Console.ReadLine()) -1;
+            Console.WriteLine((table[_index, 0])[0] + ". " + table[_index, 1] + ", " + table[_index, 2] + "g.");
         }
     }
 }
