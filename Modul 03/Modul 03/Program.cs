fd9bda8533dd81908387ea7c60a9daffb86b6258
vasilevdim <vasilevdim@gmail.com>
2017-02-21 22:22:49 +0300

    Modul 3 Video 04 -Bool Variables

--- Modul 03/Modul 03/Program.cs
+++ Modul 03/Modul 03/Program.cs
@@ -13,10 +13,18 @@ namespace Modul_03
             double a = 0, b = 0, c = 0;
         Console.Write("<a="); a = Convert.ToDouble( Console.ReadLine ());
         Console.Write ("<b="); b= Convert.ToDouble( Console.ReadLine ());
-            string_format = "#0.00 m3";
-            Console.WriteLine("\n\n>a/b=" + a / b + (a / b).ToString( _format));
+            //Formatirane s dimensia
+            
+            //Stepenuvane
+            c = Math.Pow(a, b);
+            Console.WriteLine("\n\n>a^b=" + c.ToString());
+            c = Math.Sqrt(b);
+            Console.WriteLine("\n\n>b^1/2=" + c.ToString());
+            c = Math.Round(a/b);
+            Console.WriteLine("\n\n>(a/b)=" + c.ToString());
 
-        
+
+
         }
     }
 }
