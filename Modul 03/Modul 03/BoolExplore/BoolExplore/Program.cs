cbf736b94ea6fbca53dcd2eae9e5f12ec3e2fbdd
vasilevdim <vasilevdim@gmail.com>
2017-02-23 22:43:01 +0300

    Modul 03 Video 06 - IF OPERAND

--- Modul 03/Modul 03/BoolExplore/BoolExplore/Program.cs
+++ Modul 03/Modul 03/BoolExplore/BoolExplore/Program.cs
@@ -14,16 +14,29 @@ namespace BoolExplore
             bool _test = 5 > 10;
             int[] _i = new int[] { 4, 3, 2 };
             int _ui = Convert.ToInt32(Console.ReadLine());
-           
+
             bool _check01 = _ui <= _i.Length;
             bool _check02 = _ui > 0;
             bool _exp01 = _check01 && _check02;//Logic "AND"
             bool _exp02 = _check01 || _check02;//Logic "OR"
             bool _exp03 = !_check01;
 
-            Console.WriteLine("Logic AND:"+_exp01.ToString());
-            Console.WriteLine("LOgic OR:"+ _exp02.ToString());
-            Console.WriteLine("NOT OF "+_check01.ToString()+": "+_exp03);
+            //Console.WriteLine("Logic AND:"+_exp01.ToString());
+            //Console.WriteLine("LOgic OR:"+ _exp02.ToString());
+            //Console.WriteLine("NOT OF "+_check01.ToString()+": "+_exp03);
+            if (_ui > 0 && _ui <= _i.Length)
+            {
+                Console.WriteLine(_i[_ui - 1]);
+            } else { Console.WriteLine("Invalid index"); 
+            }
+            if (_ui>_i.Length)
+            {
+                Console.WriteLine("The index is too high");
+                if (_ui <=0)
+                {
+                    Console.WriteLine("The index is too low");
+                }
+            }
         }
     }
 }
