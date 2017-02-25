250726b494f493bb9dfea0bb6d21c31af7947c43
vasilevdim <vasilevdim@gmail.com>
2017-02-25 21:46:56 +0300

    Modul03 video07

--- Modul 03/Modul 03/BoolExplore/BoolExplore/Program.cs
+++ Modul 03/Modul 03/BoolExplore/BoolExplore/Program.cs
@@ -13,28 +13,35 @@ namespace BoolExplore
             bool _b = true;
             bool _test = 5 > 10;
             int[] _i = new int[] { 4, 3, 2 };
-            int _ui = Convert.ToInt32(Console.ReadLine());
-
-            bool _check01 = _ui <= _i.Length;
-            bool _check02 = _ui > 0;
-            bool _exp01 = _check01 && _check02;//Logic "AND"
-            bool _exp02 = _check01 || _check02;//Logic "OR"
-            bool _exp03 = !_check01;
-
-            //Console.WriteLine("Logic AND:"+_exp01.ToString());
-            //Console.WriteLine("LOgic OR:"+ _exp02.ToString());
-            //Console.WriteLine("NOT OF "+_check01.ToString()+": "+_exp03);
-            if (_ui > 0 && _ui <= _i.Length)
+            int _ui = 0;//Convert.ToInt32(Console.ReadLine());
+            String _input = "0";
+            Console.Write("Please insert index:");
+            _input = Console.ReadLine();
+            bool _check01 = int.TryParse(_input, out_ui);
+            bool _check02 = _ui <= int.Lenght;
+            bool _check03 = _ui > 0;
+            if (_check01 && _check02 && _check03)
             {
                 Console.WriteLine(_i[_ui - 1]);
-            } else { Console.WriteLine("Invalid index"); 
-            }
-            if (_ui>_i.Length)
-            {
-                Console.WriteLine("The index is too high");
-                if (_ui <=0)
+
+                if (!_check01)
                 {
-                    Console.WriteLine("The index is too low");
+                    Console.WriteLine("Index has to be integer\n\n\n");
+
+                    if (!_check02)
+                    {
+                        Console.WriteLine("Index too high\n\n\n");
+
+                        if (!_check03 && _check01)
+                        {
+                            Console.WriteLine("Index too low\n\n\n");
+
+                            //Console.WriteLine("Logic AND:"+_exp01.ToString());
+                            //Console.WriteLine("LOgic OR:"+ _exp02.ToString());
+                            //Console.WriteLine("NOT OF "+_check01.ToString()+": "+_exp03);
+
+                        }
+                    }
                 }
             }
         }
