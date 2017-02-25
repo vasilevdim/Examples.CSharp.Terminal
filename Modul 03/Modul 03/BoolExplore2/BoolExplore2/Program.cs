86547de3e29a7f4b0a78158cc253ea4e4a1f1e85
vasilevdim <vasilevdim@gmail.com>
2017-02-25 22:23:36 +0300

    Modul 03 Video 08

---
+++ Modul 03/Modul 03/BoolExplore2/BoolExplore2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoolExplore2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] _i = new int[] { 4, 3, 2 };
+            int _ui = 0;
+            string _input = "0";
+            Console.Write("Please insert index:");
+            _input = Console.ReadLine();
+            bool _check01 = int.TryParse(_input, out _ui);
+
+
+            if (_check01)
+            {
+                Console.WriteLine(_i[_ui - 1]);
+
+                _ui = (_ui <= _i.Length) ? _ui : 3;
+                _ui = (_ui > 0) ? _ui : 1;
+
+
+
+            }
+        }
+    }
+}
