503f6da1dd2f43bc1350b5c5eb7bc46607b14c31
vasilevdim <vasilevdim@gmail.com>
2017-02-27 22:26:35 +0300

    Modul04 Video01 For

---
+++ Modul04/Modul04/Modul04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul04
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++);
+            { 
+                Console.WriteLine ( i.ToString());
+            }
+        }
+    }
+}
