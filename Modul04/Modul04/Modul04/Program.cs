0ccd3c84d518ee3c809f3d5fea9e0c079911f712
vasilevdim <vasilevdim@gmail.com>
2017-03-05 22:11:20 +0300

    Modul04, Video 07 - List

--- Modul04/Modul04/Modul04/Program.cs
+++ Modul04/Modul04/Modul04/Program.cs
@@ -10,10 +10,33 @@ namespace Modul04
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 5; i++);
-            { 
-                Console.WriteLine ( i.ToString());
-            }
+            //Definition
+            int i = 0;
+            string _userinput = "";
+            List<int> _List = new List<int>();
+            _List.Add(5);
+            _List.Add(3);
+            _List.Add(9);
+
+            do
+            {
+                Console.Write("? ");
+                _userinput = Console.ReadLine();
+                if (_userinput.Contains("show")) ;
+                {
+                    for (i = 0; i < _List.Count; i++)
+                    {
+                        Console.Write("_list:");
+                        Console.Write(_List[i]);
+                        if (i != _List.Count - 1) Console.Write(",");
+                    }
+                 
+
+
+                    //User's input 
+                }
+
+            } while (_userinput != "exit");
+
         }
-    }
-}
+    } }
